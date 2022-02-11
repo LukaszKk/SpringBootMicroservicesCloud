088b8d73bcc7aa9f8ec7ab475fe42d2236a305f4
Łukasz Kuk <lukasz0104201@wp.pl>
2022-02-12 00:11:01 +0100

    AccountManager microservice

--- ApiAccountManagement/src/main/java/org/spring/controller/AccountController.java
+++ ApiAccountManagement/src/main/java/org/spring/controller/AccountController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-public class UsersController {
+@RequestMapping("/account")
+public class AccountController {
 
     @GetMapping("/status")
     public String status() {
