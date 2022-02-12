28982e4ba24a879d6e8bd27221d735441aa68d23
Łukasz Kuk <lukasz0104201@wp.pl>
2022-02-12 02:17:09 +0100

    multiple same microservices with different port num

--- ApiAccountManagement/src/main/java/org/spring/controller/AccountController.java
+++ ApiAccountManagement/src/main/java/org/spring/controller/AccountController.java
@@ -1,5 +1,7 @@
 package org.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountController {
 
+    private Environment environment;
+
+    @Autowired
+    public AccountController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status")
     public String status() {
-        return "Running";
+        return "Running on port " + environment.getProperty("local.server.port");
     }
 }
